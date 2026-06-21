ed1c5ee68fe7043239e790b29a0b32633d1785c3
Aravinth <aravinth466677@gmail.com>
2026-06-21 08:25:45 +0530

    auth

--- src/services/api.js
+++ src/services/api.js
@@ -6,8 +6,12 @@ const api = axios.create({
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token")
-  console.log("TOKEN:", token);
-  if (token) {
+  
+  const isAuthRequest =
+    config.url?.includes('/auth/login') ||
+    config.url?.includes('/auth/register')
+
+  if (token && !isAuthRequest) {
     config.headers.Authorization = `Bearer ${token}`
   }
 
